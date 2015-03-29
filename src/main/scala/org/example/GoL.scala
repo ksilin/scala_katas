c45c776cf52311f1ea107e657d60d38217542b0e
ksilin <konstantin.silin@gmail.com>
2015-03-29 14:49:45 +0200

    tick: replaced for comp with filtered and mapped sets still cannot make 'collect' work properly

--- src/main/scala/org/example/GoL.scala
+++ src/main/scala/org/example/GoL.scala
@@ -20,21 +20,22 @@ object GoL {
     neighborOffsets map { (a) => ((a._1 + x) -> (a._2 + y)) }
   }
 
-  def tick(c: Set[(Int, Int)]): Set[(Int, Int)] = {
+  def willBe(state: Symbol): PartialFunction[((Int, Int), Option[Symbol]), (Int, Int)] = {
+    case p: ((Int, Int), Some[Symbol]) if state == p._2 => p._1
+  }
 
-    // checking the entire field between min and max x & y
-    // TODO - use the neighborhood of all cells - would it be more practical?
-    var deceased: Set[(Int, Int)] = Set()
-    var born: Set[(Int, Int)] = Set()
-    locationsToCheck(c) foreach { (l) =>
-      rules(countNeighbors(c, l._1, l._2)) match {
-        case None =>
-        case Some(v: Symbol) => if ('dead == v) deceased += l else born += l
-      }
-    }
+  def tick(c: Set[(Int, Int)]): Set[(Int, Int)] = {
+    val nextStates: Set[((Int, Int), Option[Symbol])] = locationsToCheck(c).map { (loc) => (loc -> rules(countNeighbors(c, loc._1, loc._2))) }
+    val deceased = nextStates filter {
+      'dead == _._2.get
+    } map (_._1)
+    val born = nextStates filter {
+      'alive == _._2.get
+    } map (_._1)
     c -- deceased ++ born
   }
 
+  // TODO - use the neighborhood of all cells - would it be more practical?
   def locationsToCheck(c: Set[(Int, Int)]): Set[(Int, Int)] = {
     val allX = c.map {_._1}
     val allY = c.map {_._2}
